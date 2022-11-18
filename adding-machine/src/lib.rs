c1f3d58345ea88966afb8e2b1ca93837f67c7250
jonathanpv <jonpad512@gmail.com>
2022-11-18 14:54:33 -0500

    adds read_string example code in rust host and guest

--- adding-machine/src/lib.rs
+++ adding-machine/src/lib.rs
@@ -1,7 +1,101 @@
+// this is going to be running in wasmtime
+// since this will be compiled to wasm32
+
+
+// this is the guest rust program
+extern crate alloc;
+use std::slice;
+use std::mem::MaybeUninit;
+use alloc::vec::Vec;
+
 #[cfg_attr(all(target_arch = "wasm32"), export_name = "add_wasm")]
 #[no_mangle]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[cfg_attr(all(target_arch = "wasm32"), export_name = "echo_string")]
+#[no_mangle]
+pub unsafe extern "C" fn echo_string(ptr: u32, len: u32) -> u32 {
+    ptr + len
+}
+
+// this will read a string that was written
+// by the runrunrun/src/main.rs file
+// returns the length of the string read
+#[cfg_attr(all(target_arch = "wasm32"), export_name = "read_string")]
+#[no_mangle]
+pub unsafe extern "C" fn read_string(ptr: u32, len: u32) -> u32 {
+    let incoming_string = &ptr_to_string(ptr, len);
+    incoming_string.len() as u32
+}
+
+#[cfg_attr(all(target_arch = "wasm32"), export_name = "return_string")]
+#[no_mangle]
+pub unsafe extern "C" fn return_string(ptr: u32, len: u32) -> u32 {
+    let incoming_string = &ptr_to_string(ptr, len);
+    let new_string = format!("Incoming: {incoming_string}\nThis string was written from adding-machine/src/lib.rs");
+    store_string_at_ptr(&new_string, ptr);
+
+    new_string.len() as u32
+}
+
+// WASM Memory-related helper functinos
+//
+// TODO explore using lol_alloc instead of default rust allocator
+/// WebAssembly export that allocates a pointer (linear memory offset) that can
+/// be used for a string.
+///
+/// This is an ownership transfer, which means the caller must call
+/// [`deallocate`] when finished.
+#[cfg_attr(all(target_arch = "wasm32"), export_name = "allocate")]
+#[no_mangle]
+pub extern "C" fn _allocate(size: u32) -> *mut u8 {
+    allocate(size as usize)
+}
+
+/// Allocates size bytes and leaks the pointer where they start.
+fn allocate(size: usize) -> *mut u8 {
+    // Allocate the amount of bytes needed.
+    let vec: Vec<MaybeUninit<u8>> = Vec::with_capacity(size);
+
+    // into_raw leaks the memory to the caller.
+    Box::into_raw(vec.into_boxed_slice()) as *mut u8
+}
+
+/// WebAssembly export that deallocates a pointer of the given size (linear
+/// memory offset, byteCount) allocated by [`allocate`].
+#[cfg_attr(all(target_arch = "wasm32"), export_name = "deallocate")]
+#[no_mangle]
+pub unsafe extern "C" fn _deallocate(ptr: u32, size: u32) {
+    deallocate(ptr as *mut u8, size as usize);
+}
+
+/// Retakes the pointer which allows its memory to be freed.
+unsafe fn deallocate(ptr: *mut u8, size: usize) {
+    // TODO - should this be Box::from_raw? (see Box::into_raw docs)
+    let _ = Vec::from_raw_parts(ptr, 0, size);
+}
+
+
+
+// WASM String-related helper functions
+/// Returns a string from WebAssembly compatible numeric types representing
+/// its pointer and length.
+unsafe fn ptr_to_string(ptr: u32, len: u32) -> String {
+    let slice = slice::from_raw_parts_mut(ptr as *mut u8, len as usize);
+    let utf8 = std::str::from_utf8_unchecked_mut(slice);
+    return String::from(utf8);
+}
+
+
+/// Stores the given string 's' at the memory location pointed to by 'ptr'
+/// This assumes no buffer overflows - here be dragons.
+unsafe fn store_string_at_ptr(s: &str, ptr: u32) {
+    // Create a mutable slice of u8 pointing at the buffer given as 'ptr'
+    // with a length of the string we're about to copy into it
+    let dest = slice::from_raw_parts_mut(ptr as *mut u8, s.len() as usize);
+    dest.copy_from_slice(s.as_bytes());
 }
 
 #[cfg(test)]
--- runrunrun/src/main.rs
+++ runrunrun/src/main.rs
@@ -1,21 +1,80 @@
+// this is the host rust program
 extern crate wasmtime;
-use std::error::Error;
+use std::{error::Error, ptr::{copy, write_bytes}};
 use wasmtime::*;
+use std::slice;
 
-const WASM_BYTES_PATH: &str = "/Users/scott.opell/dev/rust-wasm-example-proj/adding-machine/target/wasm32-unknown-unknown/debug/addingmachine.wasm";
+const WASM_BYTES_PATH: &str = "/Users/jon.padilla/Documents/scott-rust-wasmtime/rust-wasm-example-project/adding-machine/target/wasm32-unknown-unknown/debug/addingmachine.wasm";
+const BUF_SIZE: u32 = 2048;
+const WASM_PAGE_SIZE: u32 = 65536;
+
+
+unsafe fn writeToWasmMemory(outgoing: String, bufPtr: u32, memory: Memory, store: &mut Store<()>, instance_ref: &mut Instance) -> Result<(), Box<dyn Error>> {
+    if outgoing.len() as u32 > BUF_SIZE {
+        println!("runrunrun/src/main.rs::writeToWasmMemory() : outgoing string length {0} bigger than buffer {1}", outgoing.len(), BUF_SIZE);
+    }
+    
+    // let memory = instance_ref.get_export(store, "memory").unwrap().into_memory().unwrap();
+
+    let mut memory_buf = memory.data(store);
+    
+    let input_size = outgoing.len() as u32;
+
+    copy(outgoing.as_ptr(), memory_buf.as_ptr() as *mut u8, 1);
+
+    Ok(())
+}
+
+// example function using the wasmtime api to allocate memory
+unsafe fn allocateToWasmMemory(memory: Memory, store: &mut Store<()>) -> Result<(), Box<dyn Error>> {
+
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::default();
     let module = Module::from_file(&engine, WASM_BYTES_PATH)?;
     let mut store = Store::new(&engine, ());
-    let instance = Instance::new(&mut store, &module, &[])?;
+    let mut instance = Instance::new(&mut store, &module, &[])?;
+
+    // Load up our exports from the wasmtime instance
+    let memory = instance.
+        get_export(&mut store, "memory").unwrap()
+        .into_memory().unwrap();
+
+    let memory_buf = memory.data(&mut store);
 
     let add = instance
         .get_typed_func::<(i32, i32), i32>(&mut store, "add_wasm")
         .expect("`add_wasm` was not an exported function");
 
-    // And finally we can call our function! Note that the error propagation
-    // with `?` is done to handle the case where the wasm function traps.
+    // let allocate = instance.get_export(&mut store, "allocate").unwrap().into_func().unwrap();
+    let allocate = instance
+        .get_typed_func::<u32, u32>(&mut store, "allocate")
+        .expect("`allocate` was not an exported function");
+
+    let deallocate = instance
+        .get_typed_func::<(u32, u32), ()>(&mut store, "deallocate")
+        .expect("`deallocate` was not an exported function");
+
+    let allocate_leaked_ptr = allocate.call(&mut store, BUF_SIZE)?;
+    let res = memory.write(&mut store, allocate_leaked_ptr as usize, String::from("something2").as_bytes());
+    
+    // call read_string in wasm module
+    let read_string = instance
+        .get_typed_func::<(u32, u32), u32>(&mut store, "read_string")
+        .expect("`read_string was not an exported function`");
+    
+    let ans = read_string.call(&mut store, (allocate_leaked_ptr, String::from("12345678").len() as u32))?;
+    println!("wasm output: {ans}");
+    
+    unsafe{
+        let mut temp_vec = vec![0u8; BUF_SIZE as usize];
+        let slice = memory.read(&mut store, allocate_leaked_ptr as usize, &mut temp_vec).unwrap();
+        let utf8 = std::str::from_utf8_unchecked(temp_vec.as_slice());
+        println!("wasm output string: {0}", utf8);
+    }
+
     let result = add.call(&mut store, (3, 8))?;
     println!("Answer: {:?}", result);
     Ok(())
